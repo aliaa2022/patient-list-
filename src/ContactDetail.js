72f0ecc4e9f0ca98c7051f22277eb5d4ea5e6f58
Aliaa Tarek <alia.tarik@stu.eng.aswu.edu.eg>
2024-06-22 00:47:36 +0300

    the photo fixed

--- src/ContactDetail.js
+++ src/ContactDetail.js
@@ -1,14 +1,18 @@
 import React from "react";
 import user from "./images/user.png";
+import user2 from "./images/user2.png";
 
 const ContactDetail = ({ contact, onClose }) => {
   const { name, age, gender, email } = contact;
+  
+  // Determine the user image based on gender
+  const userImage = gender && gender.toLowerCase() === "f" ? user2 : user;
 
   return (
     <div className="contact-detail">
       <div className="ui card centered">
         <div className="image">
-          <img src={user} alt="user" />
+          <img src={userImage} alt="user" />
         </div>
         <div className="content">
           <div className="header">{name}</div>
